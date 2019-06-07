13d209e57458877e7ef56d986a1be95e10797ea4
Adam Ismail <adam.ismail@citrix.com>
2019-06-07 17:44:07 -0400

    Initial commit, needs cleanup. Static Rule 30 is working, seems a little slow.

---
+++ Automata/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Automata
+//
+//  Created by Ai on 6/7/19.
+//  Copyright © 2019 Ai. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var collectionView: UICollectionView!
+	
+	private let itemsPerRow: Int = 100
+	private let sectionInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+}
+
+extension ViewController: UICollectionViewDataSource {
+	
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		//Ai: Consider adding a header supplementary view
+		return 1
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		//Ai: Perhaps allow the user to adjust the cell count
+		return itemsPerRow * (itemsPerRow / 2)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "caCell", for: indexPath)
+		
+		//Ai: This needs to be cleaned up/easier to digest
+		if indexPath.item < itemsPerRow, indexPath.item == itemsPerRow / 2 {
+			cell.backgroundColor = .black
+		} else if indexPath.item - itemsPerRow > 0 {
+			let topCellIndexPath = IndexPath.init(item: indexPath.item - itemsPerRow, section: indexPath.section)
+			let topLeftCellIndexPath = IndexPath.init(item: topCellIndexPath.item - 1, section: topCellIndexPath.section)
+			let topRightCellIndexPath = IndexPath.init(item: topCellIndexPath.item + 1, section: topCellIndexPath.section)
+			
+			let topCell = collectionView.cellForItem(at: topCellIndexPath)!
+			let topLeftCell = collectionView.cellForItem(at: topLeftCellIndexPath)!
+			let topRightCell = collectionView.cellForItem(at: topRightCellIndexPath)!
+			
+			if isCellEnabled(cell: topLeftCell) != (isCellEnabled(cell: topCell) || isCellEnabled(cell: topRightCell)) {
+				cell.backgroundColor = .black
+			}
+		}
+		
+		
+		return cell
+	}
+	
+	private func isCellEnabled(cell: UICollectionViewCell) -> Bool {
+		//Ai: Make an extension of UICollectionViewCell?
+		return cell.backgroundColor == .black
+	}
+}
+
+extension ViewController: UICollectionViewDelegate {
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		print(indexPath.item + 1)
+	}
+}
+
+extension ViewController : UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let sizePerItem = view.frame.width / CGFloat(itemsPerRow)
+		return CGSize(width: sizePerItem, height: sizePerItem)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return sectionInsets
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return sectionInsets.left
+	}
+}
